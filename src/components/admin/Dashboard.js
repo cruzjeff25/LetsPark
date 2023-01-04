74d54d2a4b67e8564950503d48103903c8ef44c7
mrctrstn <95068642+mrctrstn@users.noreply.github.com>
2023-01-04 21:19:22 +0800

    update

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -6,13 +6,13 @@ const Dashboard = () => {
 		<div className='dashboard'>
 		<h1>DASHBOARD</h1>
       <div class="flexcontainer">
-		<div class="box">
+		<div class="dashbox">
 			<p>53<br/><span>Registered Accounts</span></p>
 		</div>
-		<div class="box">
+		<div class="dashbox">
 			<p>32<br/><span>Registered Spaces</span></p>
 		</div>
-		<div class="box">
+		<div class="dashbox">
 			<p>16<br/><span>Pending Spaces</span></p>
 			
 		</div>
