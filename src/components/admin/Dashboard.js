908f5f9468d1fa7b4b837c305b4d1905e3ddfd79
mrctrstn <95068642+mrctrstn@users.noreply.github.com>
2023-01-04 17:32:21 +0800

    pleksbaks

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -3,27 +3,22 @@ import "../../styles/admin/dashboard.css"
 
 const Dashboard = () => {
   return (
-    <div className='dashboard'>
-      Dashboard
-      <div class="col-div-3">
+		<div className='dashboard'>
+		<h1>DASHBOARD</h1>
+      <div class="flexcontainer">
 		<div class="box">
 			<p>53<br/><span>Registered Accounts</span></p>
-			<i class="fa fa-users box-icon"></i>
 		</div>
-	</div>
-  <div class="col-div-4">
 		<div class="box">
 			<p>32<br/><span>Registered Spaces</span></p>
-			<i class="fa fa-users box-icon"></i>
 		</div>
-	</div>
-  <div class="col-div-5">
 		<div class="box">
 			<p>16<br/><span>Pending Spaces</span></p>
-			<i class="fa fa-users box-icon"></i>
+			
 		</div>
-	</div>  
     </div>
+	</div>
+	
   )
 }
 
